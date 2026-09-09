1392f7befc5016f415260b2d5442ea3ed5f00911
elmyrockers <elmyrockers@gmail.com>
2026-09-09 23:12:23 +0800

    Move `Config` struct to correct file

--- config.go
+++ config.go
@@ -1,1 +1,15 @@
 package xvelope
+
+const (
+	CookieScheme uint8 = 1 << iota
+	BearerScheme
+	CookieAndBearerScheme = CookieScheme | BearerScheme
+)
+
+type Config struct{
+	DefaultScheme uint8
+	PolicyScheme func(ctx HttpContext) error
+	LoginRoute string
+	CookieHandler Authenticator
+	BearerHandler Authenticator
+}
--- xvelope.go
+++ xvelope.go
@@ -1,19 +1,5 @@
 package xvelope
 
-const (
-	CookieScheme uint8 = 1 << iota
-	BearerScheme
-	BothScheme = CookieScheme | BearerScheme
-)
-
-type Config struct{
-	DefaultScheme uint8
-	PolicyScheme func(ctx HttpContext) error
-	LoginRoute string
-	CookieHandler Authenticator
-	BearerHandler Authenticator
-}
-
 type Auth struct {
 	context HttpContext
 }
