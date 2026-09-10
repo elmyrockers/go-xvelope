43b35616576359feb060d740df5b60d20b679a87
elmyrockers <elmyrockers@gmail.com>
2026-09-10 23:26:15 +0800

    Update `New()` - resolve config slice

--- xvelope.go
+++ xvelope.go
@@ -1,10 +1,21 @@
 package xvelope
 
+import "github.com/davecgh/go-spew/spew"
+
 type Auth struct {
 	context HttpContext
 }
 
 func New( config ...Config) *Auth {
+	// Resolve config slice
+		var cfg Config
+		if len(config) > 0 {
+			cfg = config[0]
+		}
+
+	// 
+	spew.Dump( cfg )
+
 	return &Auth{}
 }
 
